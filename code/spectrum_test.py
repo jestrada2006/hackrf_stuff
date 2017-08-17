2930f90e871cdd99af939409f96b084f22ce011d
jestrada2006 <31011535+jestrada2006@users.noreply.github.com>
2017-08-17 15:32:37 -0400

    Update to spectrum_test.py
    created new processes for sweeps and used parent process to keep track of time before killing them

--- code/spectrum_test.py
+++ code/spectrum_test.py
@@ -1,4 +1,4 @@
-﻿################################################################################################################################################################
+################################################################################################################################################################
 #  spectrum_test.py
 #  Joseph Estrada
 #  EPRI Engineer/Scientist I
@@ -33,52 +33,57 @@
 ###############################################################################################################################################################
 
 import time
+import os
+from multiprocessing import Process
+import threading
+import signal
+import subprocess
 
-#set the frequency ranges that will be executed upon
-frequencies = [902, 928, 2400, 2500, 3566, 3700, 5170, 5838]
-#determine the current location
-#determine the hackrf commands' arguments
-commands = ["command1", "command2", "command3", "command4", "command5"]
 
-i = 0
-while i < 8:
-  #determine a filename for the file that will store the data
-  dataFileName = str(frequencies[i]) + "_to_" + str(frequencies[i+1])
-  print(dataFileName)
-  i = i + 2
+if __name__ == '__main__':
 
-  #create the log file that will be used using the data filename
-  logfile = open(dataFileName + "_log.txt", "w+")
-  #insert the location information into the log file
-  logfile.write("Location: \n")
-  #insert the commands that will be used into the log file
-  logfile.write("The following commands were executed:\n")
+  #set the frequency ranges that will be executed upon
+  frequencies = [902, 928, 2400, 2500, 3566, 3700, 5170, 5838]
+  #determine the current location
+  #determine the hackrf commands' arguments
+  commands = ["hackrf_sweep -f ", "hackrf_sweep -f", "hackrf_sweep -f", "hackrf_sweep -f"]
+  
+  i = 0
   j = 0
-  while j < 5:
-    logfile.write("     " + commands[j] + "\n")
+  while i < 8:
+    #determine a filename for the file that will store the data
+    freqFileName = str(frequencies[i]) + "_to_" + str(frequencies[i+1])
+    dataFileName = freqFileName + "_data.txt"
+  
+    #change the commands to match what we need
+    commands[j] = commands[j] + str(frequencies[i]) + ":" + str(frequencies[i+1]) + " -w 200000 -r " + dataFileName + " &"
+
+    #create the log file that will be used using the data filename
+    logfile = open(freqFileName + "_log.txt", "w+")
+    #insert the location information into the log file
+    logfile.write("Location: \n")
+    #insert the commands that will be used into the log file
+    logfile.write("The following command was executed:\n" + "     " + commands[j] + "\n")
+    
+    #calculate the start time
+    start = time.strftime("%c")
+    #execute the appropriate hackrf methods
+    proc = subprocess.Popen(commands[j], shell = True, preexec_fn=os.setsid)
+    if i == 6:
+      time.sleep(900)
+    else:
+      time.sleep(300)
+    os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+    #calculate the stop time
+    stop = time.strftime("%c")
+    #insert the filename of the data file into the log file
+    logfile.write("Data is contained in the follwoing file: " + dataFileName + "\n")
+    #insert the start time into the log file
+    logfile.write("Start Time: " + start + "\n")
+    #insert the stop time into th elog file
+    logfile.write("Stop Time: " + stop + "\n")
+    #close the log file
+    logfile.close()
+
+    i = i + 2
     j = j + 1
-  #calculate the start time
-  start = time.strftime("%c")
-  #execute the appropriate hackrf methods
-  #calculate the stop time
-  stop = time.strftime("%c")
-  #insert the filename of the data file into the log file
-  logfile.write("Data is contained in the follwoing file: " + dataFileName + "_data.txt\n")
-  #insert the start time into the log file
-  logfile.write("Start Time: " + start + "\n")
-  #insert the stop time into th elog file
-  logfile.write("Stop Time: " + stop + "\n")
-  #close the log file
-  logfile.close()
-#change the sweep characteristics
-#do it again until we no longer want to
-
-
-#currentTime = datetime.datetime.now()
-
-
-
-
-
-
-
